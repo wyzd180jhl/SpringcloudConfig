35528bac981466f0461c9e46e63f53a490c2a75e
Bruce <wyzd180jhl@foxmail.com>
2019-05-04 10:16:50 +0800

    second commit

---
+++ config-server/src/main/java/com/bruce/configclient/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.bruce.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: LJ
+ * @Date: 2019/5/4 09:23
+ * @Description:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
